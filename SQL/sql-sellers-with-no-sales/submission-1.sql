b13eb2daaf6cc7005f39f0d8c2e30598bd17e14a
Shati Mondal <161274658+shati-mondal@users.noreply.github.com>
2026-08-13 13:49:45 +0530

    Add: sql-sellers-with-no-sales - submission-1

---
+++ SQL/sql-sellers-with-no-sales/submission-1.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+SELECT seller_name
+FROM Seller
+WHERE seller_id NOT IN (
+    SELECT seller_id
+    FROM Orders
+    WHERE EXTRACT (YEAR FROM sale_date) = 2020
+)
+ORDER BY seller_name ASC;
